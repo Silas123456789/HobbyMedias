16b257e47044ad17fde327c0082401ed58c46400
Silas <156440775+Silas123456789@users.noreply.github.com>
2024-01-27 12:12:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function openPopup() {
+    document.getElementById('standortPopup').style.display = 'block';
+}
+
+function closePopup() {
+    document.getElementById('standortPopup').style.display = 'none';
+}
+
+window.openPopup = openPopup;
+window.closePopup = closePopup;
+
